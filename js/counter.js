8de4d234fc15a599a3d0fabcdd590c668a772f44
JamieChen007 <107536976+JamieChen007@users.noreply.github.com>
2023-09-06 16:47:16 +1000

    update counter.js

--- js/counter.js
+++ js/counter.js
@@ -1,6 +1,7 @@
 const twitterFollowers = document.querySelector(".twitterFollowers");
 const youtubeFollowers = document.querySelector(".youtubeFollowers");
 const facebookFollowers = document.querySelector(".facebookFollowers");
+const socialMedia = document.querySelector(".socialMedia");
 
 let currNum;
 let target;
@@ -15,6 +16,16 @@ const count = (currNum, target, el) => {
   }, 1);
 };
 
-count(10465, 12000, twitterFollowers);
-count(3254, 5000, youtubeFollowers);
-count(5423, 7500, facebookFollowers);
+let options = {
+  root: null,
+  rootMargin: "0px",
+  threshold: [0, 1],
+};
+
+let io = new IntersectionObserver(() => {
+  count(10465, 12000, twitterFollowers);
+  count(3254, 5000, youtubeFollowers);
+  count(5423, 7500, facebookFollowers);
+}, options);
+
+io.observe(socialMedia);
